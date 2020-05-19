0df851ff66009da4f4de302f8e84e4ec6e534d4d
Nick Bradshaw <nickalasb@gmail.com>
2020-05-18 22:01:10 -0700

    Firebase-8: Delete unused cloudstore functions

--- lib/firebase/base_cloudstore.dart
+++ lib/firebase/base_cloudstore.dart
@@ -14,6 +14,50 @@ class Cloudstore {
     return _fbCloudstore.collection(path);
   }
 
+  Future<void> updateCollectionData(
+    CollectionReference collection,
+    String documentPath,
+    Map<String, dynamic> data,
+  ) async {
+    await collection.document(documentPath).updateData(data);
+  }
+
+  Future<void> deleteSingleGame(BuildContext context, String pin) async {
+    final user = await Auth.of(context).getCurrentUser();
+    final singleGamesCollectionPath = 'accounts/${user.uid}/single_games';
+
+    await collection(singleGamesCollectionPath).document(pin).delete();
+  }
+
+  Future<CollectionReference> singleGameCollectionReference(
+    BuildContext context,
+  ) async {
+    final user = await Auth.of(context).getCurrentUser();
+    return collection('accounts/${user.email}/single_games');
+  }
+
+  Future<void> createSingleGameCollection(
+    BuildContext context, {
+    String pin,
+  }) async {
+    final user = await Auth.of(context).getCurrentUser();
+    final gameCollectionReference =
+        await singleGameCollectionReference(context);
+
+    final allDocs = await gameCollectionReference.getDocuments();
+    final gamePaths = allDocs.documents.map((d) => d.reference.path);
+
+    if (gamePaths.contains('$gameCollectionReference/$pin')) {
+      throw Exception();
+    } else {
+      await collection('accounts')
+          .document(user.email)
+          .collection('single_games')
+          .document(pin)
+          .setData(gameElements);
+    }
+  }
+
   Map<String, dynamic> gameElements = {
     'player1Name': '',
     'player1Score': 0,
@@ -25,78 +69,4 @@ class Cloudstore {
     'numberOfRounds': null,
     'insult': ''
   };
-
-  Future<void> setCollectionData({
-    CollectionReference collection,
-    String documentPath,
-    Map<String, dynamic> data,
-  }) async {
-    await collection.document(documentPath).setData(data);
-  }
-
-  Future<void> updateCollectionData(
-    CollectionReference collection,
-    String documentPath,
-    Map<String, dynamic> data,
-  ) async {
-    await collection.document(documentPath).updateData(data);
-  }
-
-  Future<void> post(
-    CollectionReference collectionRef, {
-    @required Map<String, dynamic> newData,
-  }) async {
-    _fbCloudstore.runTransaction((tx) async {
-      final allDocs = await collectionRef.getDocuments();
-      final toBeRetrieved =
-          allDocs.documents.sublist(allDocs.documents.length ~/ 2);
-      final toBeDeleted =
-          allDocs.documents.sublist(0, allDocs.documents.length ~/ 2);
-      await Future.forEach(toBeDeleted, (DocumentSnapshot snapshot) async {
-        await tx.delete(snapshot.reference);
-      });
-
-      await Future.forEach(toBeRetrieved, (DocumentSnapshot snapshot) async {
-        await tx.update(snapshot.reference, newData);
-      });
-    });
-  }
-
-  Future<void> deleteDocument(
-    CollectionReference collectionRef,
-    String documentPath,
-  ) async {
-    _fbCloudstore.runTransaction((tx) async {
-      await tx.delete(collectionRef.document(documentPath));
-    });
-  }
-
-  Future<void> deleteSingleGame(BuildContext context, String pin) async {
-    final user = await Auth.of(context).getCurrentUser();
-    final singleGamesCollectionPath = 'accounts/${user.uid}/single_games';
-
-    await collection(singleGamesCollectionPath).document(pin).delete();
-  }
-
-  Future<void> createSingleGameCollection(
-    BuildContext context, {
-    String pin,
-  }) async {
-    final user = await Auth.of(context).getCurrentUser();
-    final singleGamesCollectionPath = 'accounts/${user.email}/single_games';
-    final singleGamesCollection = collection(singleGamesCollectionPath);
-    final allDocs = await singleGamesCollection.getDocuments();
-
-    final gamePaths = allDocs.documents.map((d) => d.reference.path);
-
-    if (gamePaths.contains('$singleGamesCollectionPath/$pin')) {
-      throw Exception();
-    } else {
-      await collection('accounts')
-          .document(user.email)
-          .collection('single_games')
-          .document(pin)
-          .setData(gameElements);
-    }
-  }
 }
